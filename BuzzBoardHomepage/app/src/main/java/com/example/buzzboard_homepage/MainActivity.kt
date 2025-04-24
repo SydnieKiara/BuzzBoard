131dcad9a8af4d781562941ccdf8a86ee666f36a
sydnie johnson <sjoh1106@student.vsu.edu>
2025-04-24 17:39:15 -0400

    loginscreen

--- BuzzBoardHomepage/app/src/main/java/com/example/buzzboard_homepage/MainActivity.kt
+++ BuzzBoardHomepage/app/src/main/java/com/example/buzzboard_homepage/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.BuildConfig
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +19,11 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+            insets}
+
+
+
     }
-}
+
+
+    }
